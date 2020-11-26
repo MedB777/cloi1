ceab09f1a0238ee7d82ca116daabe1f82a3bc9da
Med <mohamed.bouasria.777@gmail.com>
2020-11-26 17:13:37 +0100

    Initial Commit 3 - Rotating Ads 21

--- app.js
+++ app.js
@@ -10,45 +10,6 @@ OneSignal.push(function () {
 })
 
 // Los Pollos Smartlink
-var offersList = [
-	{
-		vertical: 'Dating',
-		url: 'https://best-dating4u.com/?u=fy1nn7x&o=mbepd9x&t=cloi1'
-	},
-	{
-		vertical: 'Crypto',
-		url: 'https://profitisthere1.life/?u=fy1nn7x&o=mbmpd9x&t=cloi1'
-	},
-
-	{
-		vertical: 'Male Enhancement',
-		url: 'https://truemaleenchance1.life/?u=fy1nn7x&o=mbhpd9x&t=cloi1'
-	},
-	{
-		vertical: 'Mainstream',
-		url: 'https://your-best-prize-boxes.life/?u=fy1nn7x&o=mbkpd9x&t=cloi1'
-	},
-	{
-		vertical: 'Adult Games',
-		url: 'https://the-adult-lovers1.com/?u=fy1nn7x&o=mbtpd9t&t=cloi1'
-	},
-	{
-		vertical: 'Gambling',
-		url: 'https://prizesforyouhere.life/?u=fy1nn7x&o=mbdpd91&t=cloi1'
-	},
-	{
-		vertical: 'Gay Dating',
-		url: 'https://newhotmen.com/?u=fy1nn7x&o=mbrpd91&t=cloi1'
-	},
-	{
-		vertical: 'Casual Dating',
-		url: 'https://new-dates.com/?u=fy1nn7x&o=mbbpd91&t=cloi1'
-	},
-	{
-		vertical: 'Cams',
-		url: 'https://bestyourcams.com/?u=fy1nn7x&o=mbppd91&t=cloi1'
-	}
-]
 
 // CPA Offers
 var cpaOffersList = [
