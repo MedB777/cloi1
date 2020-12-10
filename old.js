9c5a10a57ec9a68690d077618b0efec645a89941
Med <mohamed.bouasria.777@gmail.com>
2020-12-10 13:48:08 +0100

    Initial Commit 3 - Rotating Ads 40

---
+++ old.js
@@ -0,0 +1,53 @@
+var CBOffers = [
+	{
+		id: 'biorhythm',
+		url: 'https://69c1ekg4dw8xfxf2tbx16gvhf1.hop.clickbank.net/',
+		name: 'S T O P - Forget All About Manifestation And Numerology...',
+		affpage: 'https://thebiorhythm.com/jv',
+		payout: 39,
+		title: 'Powerful App Predicts the Future. Works For Everyone',
+		desc:
+			'Little Known Discoveries and Secrets that Will Give You Insights into What is Heading Your Way in 2021 ...'
+	},
+	{
+		id: 'hissecret',
+		url: 'https://a789afp69v9uan4axgu1dfon8s.hop.clickbank.net/',
+		name:
+			'His Secret Obsession - Incredible Conversions For Female Cold Traffic',
+		affpage: 'https://HisSecretObsession.com/aff',
+		payout: 51,
+		title: 'Become His Secret Obsession!',
+		desc:
+			'How to Make ANY Man Obssessed with YOU - Proven Strategies and Secrets to Make Him Think Only About YOU. Reserve Your Sport Today!'
+	},
+	{
+		id: 'resurge',
+		url: 'https://890cbeg-c08v9k7eqexoo99l7w.hop.clickbank.net/',
+		name: 'Resurge - The Godzilla Of Offers',
+		affpage: 'https://resurge.com/affiliates',
+		payout: 119,
+		title: 'Unbelievable New Secrets Revealed That Will Make You a New Person',
+		desc:
+			'You Will Not Believe What This New Discoveries Show - This is Life Changing...'
+	},
+	{
+		id: 'fbtonic',
+		url: 'https://f4522gn6ip3o8t03pm3hx9bp5t.hop.clickbank.net/',
+		name: 'Okinawa Flat Belly Tonic',
+		affpage: 'http://flatbellytonic.com/affiliates/',
+		payout: 126,
+		title: 'Drinking This Before Breakfast Burns 1lb a Day?',
+		desc:
+			'Discover the “Done for You” Japanese Tonic to Melt 3 Pounds Every 3.5 Days'
+	},
+	{
+		id: 'tedsplans',
+		url: 'https://7f371lhzbwbm8magj65dpyug79.hop.clickbank.net/',
+		name:
+			'Tedswoodworking - Highest Converting Woodworking Site On The Internet!',
+		affpage: 'http://tedswoodworking.com/aff',
+		payout: 62,
+		title: 'The Largest Collection of Wood Plans!',
+		desc: 'Watch This Video Till The End To Get Full Access!'
+	}
+]
